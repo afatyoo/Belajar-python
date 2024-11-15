4c65815e0381a5e86f4a268df13f343e02575b8e
afatyoo <afatyo.ajeung@gmail.com>
2024-11-15 15:38:37 +0700

    Update break.py

---
+++ code control flow/break.py
@@ -0,0 +1,14 @@
+angka = 0
+
+print(f"angka sekarang -> {angka}")
+
+while angka < 5:
+    angka += 1
+    print(f"angka sekarang -> {angka}")
+
+    if angka == 3:
+        print("nice!") 
+        break
+    print("whassup!")
+
+print("Pinish!")
