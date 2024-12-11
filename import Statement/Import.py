c4b43c9d9deb26624e70895a32d97f98997575aa
afatyoo <afatyo.ajeung@gmail.com>
2024-12-11 14:02:43 +0700

    update

---
+++ import Statement/Import.py
@@ -0,0 +1,6 @@
+# import 
+
+# fungsinya mengambil program dari file external python
+import program_print as pt
+
+
---
+++ import Statement/package.py
@@ -0,0 +1,12 @@
+import math.matematika
+
+hasil_tambah = math.matematika.tambah(1,2,3,4,5)
+print(f"berikut adalah hasil  dari tambah = {hasil_tambah}")
+
+
+#hasil_kali = math.matematika.kali(1,2,3,4,5)
+#print(f"berikut adalah hasil  dari kali = {hasil_kali}")
+#
+#
+#hasil_pangkat = math.matematika.pangkat(1,2,3,4,5)
+#print(f"berikut adalah hasil  dari pangkat = {hasil_pangkat}")
